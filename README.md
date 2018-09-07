aaea50143942e8ffa3eff8540699dd1be904f8ba
Ramona Saintandre <RamonaS@vaneerden.com>
2018-09-07 15:23:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# David's Memory Game
+This has been a long journey.
+ 
+ 
+Enjoy the game 
+
+# How to Play the Game 
+
+The game starts when you click the fist card. 
+After you make 8 matches, you will get a surprise 
+
+# Resources used for this projects Please refer to 
+[resources](https://github.com/thenewmona/GWG-memoryGame/blob/master/resources.md)
+
+# Issues 
+
+ 
+
+
+# Created by Ramona Saintandre 
+
+
+
+
---
+++ js/mem2.js
@@ -0,0 +1,242 @@
+const cards = document.querySelectorAll('.memory-card');
+const restartBtn = document.querySelector('resetBtn');
+let hasFlippedCard = false;
+let lockBoard = false; //this is so the cards won't flip back once they are matched 
+let firstCard, secondCard;
+let timeStart = "";
+let matchCount = 0; //added by Carlos
+//let resetBtn;
+
+//lets us know which card the player clicked so that we can do the match 
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return; //https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval#The_this_problem
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) { //first click !
+        hasFlippedCard = true;
+        firstCard = this;
+        startTimer();
+        return;
+
+    }
+
+    //second click        
+    secondCard = this;
+    checkForMatch();
+    addMove();
+    removeStars();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card; //this is if its a match
+    isMatch ? disableCards() : unflipCards();
+
+}
+
+//if => 8 modal popup still need to configure this 
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard); //remove the eventListener if it's a match. You have to add the event and the function that you called 
+    secondCard.removeEventListener('click', flipCard); //remove the eventListener if it's a match
+    resetCard();
+    matchCount++;
+    console.log(matchCount);
+    //if matchCount equals 8, all cards have been successfully matched and the game is over.
+    if (matchCount >= 8) {
+        gameOver();
+
+    }
+
+}
+
+function gameOver() {
+    stopTimer();
+    openModal();
+    applause.play();
+}
+
+// if they don't match 
+function unflipCards() {
+    lockBoard = true;
+    noMatch.play();
+
+    setTimeout(() => {
+        //so that we can see the card flip
+        // setTimeout excutes after waiting a specified amount of time
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetCard();
+
+    }, 700);
+}
+
+function resetCard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    });
+}
+
+let moves = 0;
+
+function addMove() {
+    moves++;
+    const movesText = document.querySelector('.moves');
+    movesText.innerHTML = moves;
+}
+if (hasFlippedCard.length === 2) {
+    //checkForMatch(clickTarget);
+
+}
+
+function removeStars() {
+    if (moves === 10 || moves === 20 || moves === 30) {
+        removeStar();
+
+    }
+}
+
+function removeStar() {
+    let starsList = document.querySelectorAll('.stars li');
+    for (stars of starsList) {
+        if (stars.style.display !== 'none') {
+            stars.style.display = 'none';
+            break; //need to find out what break does 
+
+        }
+    }
+}
+removeStars();
+
+//timer 
+//let stopTimer = false;//need this for the timer and the reset 
+let resetGame = true; //need this for game reset and modal 
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+
+function restartGame() {
+    if (timeStart) stopTimer();
+    resetTimer();
+    cards.forEach(card => {
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard);
+    });
+    shuffle();
+    matchCount = 0;
+    moves = 0;
+    const movesText = document.querySelector('.moves');
+    movesText.innerHTML = moves;
+    yeah.play();
+}
+//restartBtn = document.getElementsByClassName("resetBtn");
+
+// restartBtn.addEventListener('click', function(e) {
+// });
+//     restartBtn = document.getElementsByClassName("resetBtn");
+
+function startTimer() {
+    if (resetGame == true) {
+        let timer = 0;
+        if (timeStart === "") {
+            timeStart = setInterval(() => { //8-30 if i use => timer works, change it per https://www.w3schools.com/js/js_timing.asp it acts like it wants to start but doesnt
+                ++timer;
+                second = timer % 60;
+                minute = Math.floor(timer / 60);
+                if (minute < 10) minute = '0' + minute;
+                if (second < 10) second = '0' + second;
+                //hour = Math.floor(timer / 3600);
+                //minute = Math.floor((timer - hour * 3600) / 60);
+                //second = timer - hour * 3600 - minute * 60;
+                //if (hour < 10) hour = '0' + hour;
+                //if (minute < 10) minute = '0' + second;
+                document.querySelector(".timer").innerHTML = minute + ':' + second;
+                document.querySelector(".clock").innerHTML = minute + ':' + second;
+            }, 1000);
+        }
+    }
+}
+
+function resetTimer() {
+    document.querySelector(".timer").innerHTML = '00:00';
+    [hour, minute, second] = [0, 0, 0];
+}
+
+function stopTimer() {
+    clearInterval(timeStart); //clearInterval needs to use the variable from the setInterval 
+    timeStart = '';
+}
+//get modal element
+
+
+let modal = document.getElementById('simpleModal');
+
+
+//get close button 
+
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+
+//Listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+//listen for outside click
+
+window.addEventListener('click', outsideClick);
+
+//function to open Modal
+function openModal() {
+    modal.style.display = 'block'; // this is rendered as a block level element 
+}
+
+//function to close Modal
+function closeModal() {
+    modal.style.display = 'none'; //element will not be displayed 
+}
+//function to close Modal if outside click
+function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+// 9-4 Reset Stars with help from Danny Smith from midwest_Fend cohort
+function resetStars() {
+    let starsList = document.querySelectorAll('.stars li');
+    for (stars of starlist) {
+        stars.style.display = 'block';
+    }
+}
+//9-4reset game button with help from Danny Smith 9-4
+let resetBtn =
+    document.getElementById('resetBtn')
+resetBtn.addEventListener('click', resetPlay, false)
+
+function resetPlay() {
+    stopTimer();
+    resetTimer();
+    resetMoves();
+    resetStars();
+    
+}
+// 9-4 fixed my moves function with the help of Danny 
+function resetMoves() {
+    const movesText = document.querySelector('moves');
+    let moves = 0;
+    movesText.innerHTML = moves;
+
+}
+let noMatch = new Audio("audio/noMatch.mp3");
+let applause = new Audio("audio/Ovation.mp3");
+let match = new Audio("audio/match.mp3");
+let yeah = new Audio("audio/match.mp3");
+shuffle();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
